1b64090b32582df5dddaacc8559855fbcb07a708
artem <arte@kb.ri>
2022-04-11 22:26:11 +0300

    done struct

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let example = Story(title: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = "You see a fork in the road."
+        choice1Button.setTitle("Take a left", for: .normal)
+        choice2Button.setTitle("Take a right", for: .normal)
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
 
+//        choice1Button.currentTitle = "Take a left"
+//        choice2Button.currentTitle = "Take a right"
+    }
+    
 }
 
